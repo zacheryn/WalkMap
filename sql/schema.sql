e6789e0d41a3de01d8b89238d395683cf50f3d7f
Zachery Nagy <zacheryn@umich.edu>
2024-09-19 09:58:43 -0400

    schema finished (not tested)

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,3 +1,5 @@
+PRAGMA foreign_keys = ON;
+
 CREATE TABLE Users(
     user_id INTEGER PRIMARY KEY AUTOINCREMENT,
     username VARCHAR(40) NOT NULL,
@@ -14,4 +16,32 @@ CREATE TABLE Locations(
     state_name VARCHAR(100) NOT NULL,
     city_name VARCHAR(100) NOT NULL,
     [address] VARCHAR(100)
-)
+);
+
+CREATE TABLE Reviews(
+    review_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    content VARCHAR(2000),
+    overall REAL NOT NULL,
+    sidewalk_quality INTEGER,
+    slope INTEGER,
+    road_dist INTEGER,
+    sidewalk INTEGER CHECK(sidewalk IN (0, 1)),
+    public_trans INTEGER CHECK(public_trans IN (0, 1)),
+    created DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE OwnsReview(
+    user_id INTEGER NOT NULL,
+    review_id INTEGER NOT NULL,
+    PRIMARY KEY(user_id, review_id),
+    FOREIGN KEY(user_id) REFERENCES Users(user_id) ON DELETE CASCADE,
+    FOREIGN KEY(review_id) REFERENCES Reviews(review_id) ON DELETE CASCADE
+);
+
+CREATE TABLE ReviewLoc(
+    location_id INTEGER NOT NULL,
+    review_id INTEGER NOT NULL,
+    PRIMARY KEY(location_id, review_id),
+    FOREIGN KEY(location_id) REFERENCES Locations(location_id) ON DELETE CASCADE,
+    FOREIGN KEY(review_id) REFERENCES Reviews(review_id) ON DELETE CASCADE
+);
