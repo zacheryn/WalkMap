2c2953b03683d6d2349d1ea434d54ff3eb08b832
Zachery Nagy <zacheryn@umich.edu>
2024-10-23 19:04:53 -0400

    Finished retrieve_locations.py

--- MDE/api/retrieve_locations.py
+++ MDE/api/retrieve_locations.py
@@ -7,14 +7,11 @@ import MDE.model
 @MDE.app.route('/api/location/list/', methods=['GET'])
 def retrieve_locations():
     """Returns a list of all locations within a range of coordinates."""
-    # So that decimal numbers can be sent, they will be sent from a JSON
-    data = flask.request.get_json()
-
-    # Check that all needed values are present
-    if 'latmin' not in data \
-       or 'latmax' not in data \
-       or 'longmin' not in data \
-       or 'longmax' not in data:
+    # Check that all needed args are present
+    if 'latmin' not in flask.request.args \
+       or 'latmax' not in flask.request.args \
+       or 'longmin' not in flask.request.args \
+       or 'longmax' not in flask.request.args:
         error_msg = {
             "message": "Bad Request",
             "status_code": 400
@@ -33,12 +30,13 @@ def retrieve_locations():
         "AND longitude < ? "
         "AND latitude > ? "
         "AND latitude < ?",
-        (data.get('longmin'), data.get('longmax'),
-         data.get('latmin'), data.get('latmax'))
+        (float(flask.request.args.get('longmin')), float(flask.request.args.get('longmax')),
+         float(flask.request.args.get('latmin')), float(flask.request.args.get('latmax')))
     )
 
     # Take the results of the query and put them into a LIST
     locations = cur.fetchall()
+    print(locations)
 
     # take the results and put them into a context DICT
     context = {
--- MDE/js/app.jsx
+++ MDE/js/app.jsx
@@ -2,28 +2,73 @@ import React, { useState, useEffect } from "react";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import utc from "dayjs/plugin/utc";
-import { MapContainer, TileLayer, Marker, useMapEvents } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, useMapEvents, useMap } from "react-leaflet";
 
 dayjs.extend(relativeTime);
 dayjs.extend(utc);
 
-function makeACall(bounds, zoom) {
-    console.log(`Current map zoom is ${zoom}`);
-    
-    console.log("make a call to the server with the bounds:", bounds);
-}
-
-const MapEvents = () => {
-    const map = useMapEvents({
-        moveend: () => makeACall(map.getBounds(), map.getZoom()),
-        zoomend: () => makeACall(map.getBounds(), map.getZoom())
-    });
-    return null;
-}
+var first = false
 
 export default function App() {
+    const [locationsState, setLocationsState] = useState([])
+    
+    function GetLocations(bounds) {
+        let ignoreStaleRequest = false;
+        fetch("/api/location/list/?latmin=" + bounds._southWest.lat.toString()
+              + "&latmax=" + bounds._northEast.lat.toString()
+              + "&longmin=" + bounds._southWest.lng.toString()
+              + "&longmax=" + bounds._northEast.lng.toString(), {
+            method: "GET",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+            credentials: "same-origin"
+        })
+        .then((response) => {
+            if (!response.ok) throw Error(response.statusText);
+            return response.json();
+        })
+        .then((data) => {
+            if (!ignoreStaleRequest) {
+                // this had was data?.status_code before
+                if (data.status_code === undefined) {
+                    setLocationsState(data.locations);
+                }
+            }
+        })
+        .catch((error) => console.log(error));
+
+        return () => {
+            ignoreStaleRequest = true;
+        };
+    }
+    
+    function FirstBounds() {
+        if(!first){
+            console.log("first")
+            first = true
+            const map = useMap()
+            GetLocations(map.getBounds())
+        }
+        return null
+    }
+    
+    function MapEvents() {
+        console.log("move")
+        const map = useMapEvents({
+            moveend: () => GetLocations(map.getBounds()),
+            zoomend: () => GetLocations(map.getBounds())
+        });
+        return null;
+    }
     return (
-        <MapContainer center={[42.27976830712081, -83.74467699423975]} zoom={13} doubleClickZoom={false}>
+        <MapContainer
+          center={[42.27976830712081, -83.74467699423975]}
+          zoom={13}
+          doubleClickZoom={false}
+        >
+            <FirstBounds />
             <MapEvents />
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
