8121ec309744ad69c31530e885a52d247c31d82f
Zachery Nagy <122123621+zacheryn@users.noreply.github.com>
2025-01-14 17:21:41 -0500

    Delete example directory

--- example/api_example/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Insta485 REST API."""
-
-from insta485.api.posts import get_post
-from insta485.api.posts import get_ten_posts
-from insta485.api.likes import post_likes_api
-from insta485.api.likes import delete_likes_api
-from insta485.api.comments import del_comments
-from insta485.api.comments import add_comments
-from insta485.api.base import return_base
